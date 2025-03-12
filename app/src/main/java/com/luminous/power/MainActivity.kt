4dd4d9d2a3e41ab48f3304db5b8e2b262c61bf6b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-03-12 07:31:10 +0600

    remove unnecessary codes

--- app/src/main/java/com/luminous/power/MainActivity.kt
+++ app/src/main/java/com/luminous/power/MainActivity.kt
@@ -3,11 +3,6 @@ package com.luminous.power
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.luminous.power.ui.theme.PowerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
