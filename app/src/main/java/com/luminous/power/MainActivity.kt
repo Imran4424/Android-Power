3e06405d53099d016424c056276102c20edc0805
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-03-16 12:06:19 +0600

    add alert diaglog to user consent

--- app/src/main/java/com/luminous/power/MainActivity.kt
+++ app/src/main/java/com/luminous/power/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.luminous.power
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -10,11 +11,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         if (PowerMenuAccessibilityService.instance != null) {
             PowerMenuAccessibilityService.instance?.showPowerMenu()
+            finish()
         } else {
-            Toast.makeText(this, "Please enable Accessibility Service", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "Please enable Accessibility Service", Toast.LENGTH_SHORT).show()
+//            startActivity(Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS))
+            showTermsDialog()
+        }
+    }
+
+    private fun showTermsDialog() {
+        // Build the alert dialog
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Accessibility Permission")
+        builder.setMessage("Please give us accessibility permission. We need this permission to show power menu.")
+        builder.setCancelable(false) // Prevent dismissing by tapping outside or using back button
+
+        builder.setPositiveButton("Accept") { dialog, _ ->
+            dialog.dismiss()
+            // Continue with the app as needed
             startActivity(Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS))
+            finish()
         }
 
-        finish()
+        builder.setNegativeButton("Decline") { dialog, _ ->
+            // Optionally inform the user and then close the app
+            dialog.dismiss()
+            finish() // End the activity/app if terms are declined
+        }
+
+        // Create and show the dialog
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
