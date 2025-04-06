eba76b870d3f51a88104bba405cc2a9cad781eda
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-04-07 01:33:23 +0600

    make the tile always active

--- app/src/main/java/com/luminous/power/VolumeTileService.kt
+++ app/src/main/java/com/luminous/power/VolumeTileService.kt
@@ -2,6 +2,7 @@ package com.luminous.power
 
 import android.content.Context
 import android.media.AudioManager
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class VolumeTileService : TileService() {
@@ -10,5 +11,13 @@ class VolumeTileService : TileService() {
         val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         // This call nudges the system to display the volume UI without changing the volume level.
         audioManager.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI)
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile?.apply {
+            state = Tile.STATE_ACTIVE
+            updateTile()
+        }
     }
 }
